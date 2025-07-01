26ae433bc8763235bbf11a1ae67fc65eac9e1ac6
abikesa <144293697+abikesa@users.noreply.github.com>
2025-07-01 09:23:45 -0400

    Update video.sh

--- video.sh
+++ video.sh
@@ -1,5 +1,2 @@
-# Convert to gif (good for short clips)
-ffmpeg -i nonself-self-ubuntu.MOV -vf "scale=480:-1" -r 10 nonself.gif
-
-# Or to mp4 (for external hosting or GitHub Pages)
-ffmpeg -i nonself-self-ubuntu.MOV -vcodec libx264 -crf 25 nonself.mp4
+ffmpeg -i "./images/nonself-self-ubuntu.MOV" -vcodec libx264 -crf 25 nonself.mp4
+ffmpeg -i images/nonself-self-ubuntu.MOV -vf "scale=480:-1" -r 10 images/nonself.gif
